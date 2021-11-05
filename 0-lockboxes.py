5ac53adc902a815aad28c3380e00e49560cd0224
ssun97 <sunmingyu.97@gmail.com>
2021-11-05 12:29:58 -0500

    ignore file

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -2,10 +2,6 @@
 """ Interview questions collection """
 
 
-
-import itertools
-
-
 def canUnlockAll(boxes):
     """ You have n number of locked boxes in front of you. Each box is numbered sequentially
     from 0 to n - 1 and each box may contain keys to the other boxes. Write a method that
@@ -22,4 +18,3 @@ def canUnlockAll(boxes):
             if key2 not in key_list and key2 in range(1, len(boxes)):
                 key_list.append(key2)
     return all(box_opened in key_list for box_opened in range(1, len(boxes)))
-    
