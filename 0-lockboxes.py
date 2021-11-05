e1113b30d6731f91e81ff1ac8effda9153f3ed92
ssun97 <sunmingyu.97@gmail.com>
2021-11-05 12:16:44 -0500

    Task 0

---
+++ 0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Interview questions collection """
+
+
+
+import itertools
+
+
+def canUnlockAll(boxes):
+    """ You have n number of locked boxes in front of you. Each box is numbered sequentially
+    from 0 to n - 1 and each box may contain keys to the other boxes. Write a method that
+    determines if all the boxes can be opened. """
+
+    key_list = []
+
+
+    for key in boxes[0]:
+        if key in range(1, len(boxes)):
+            key_list.append(key)
+    for key in key_list:
+        for key2 in boxes[key]:
+            if key2 not in key_list and key2 in range(1, len(boxes)):
+                key_list.append(key2)
+    return all(box_opened in key_list for box_opened in range(1, len(boxes)))
+    
