204c6539e07638a8a3b38dd5316a1b015347cd0d
ssun97 <sunmingyu.97@gmail.com>
2021-12-01 16:01:01 -0600

    Task 0

---
+++ 0x03-sandpiles/0-sandpiles.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include "sandpiles.h"
+#include <stdbool.h>
+
+/**
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ *
+ */
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void split(int grid[3][3]){
+    int i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            if (grid[i][j] > 3){
+                grid[i][j] -= 4;
+                if ((i - 1) >= 0)
+                    grid[i - 1][j] += 1;
+                if ((i + 1) <= 2)
+                    grid[i + 1][j] += 1;
+                if ((j - 1) >= 0)
+                    grid[i][j - 1] += 1;
+                if ((j + 1) <= 2)
+                    grid[i][j + 1] += 1;                
+            }
+        }
+    }
+}
+
+int check_stable(int grid[3][3]){
+    int i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3){
+                return(1);
+            }
+        }
+    }
+    return(0);
+}
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++){
+            grid1[i][j] += grid2[i][j];
+        }
+    }
+    printf("=\n");
+    print_grid(grid1);
+
+    while(check_stable(grid1)){   
+        split(grid1);
+        if(check_stable(grid1)){
+            printf("=\n");
+            print_grid(grid1);
+        }
+    }
+}
