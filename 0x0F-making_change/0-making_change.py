383e5eb4a3269cf43f85ce43566d6da915895ee8
ssun97 <sunmingyu.97@gmail.com>
2022-04-28 16:33:35 -0500

    making change

---
+++ 0x0F-making_change/0-making_change.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    """
+    Function for making change
+    """
+    if total <= 0:
+        return 0
+
+    placeholder = total + 1
+
+    memo = {0: 0}
+
+    for i in range(1, total + 1):
+        memo[i] = placeholder
+
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+
+            memo[i] = min(memo[current] + 1, memo[i])
+
+    if memo[total] == total + 1:
+        return -1
+
+    return memo[total]
+    
